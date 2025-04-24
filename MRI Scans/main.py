fb105c8df2bb379efc8d0fb12dc432fa3f2db8ca
UsmanCh1226 <usmanch@umich.edu>
2025-04-23 20:38:50 -0400

    Downloaded Kaggle API

--- MRI Scans/main.py
+++ MRI Scans/main.py
@@ -1,6 +1,12 @@
-import kagglehub
+import kaggle
+import numpy as np
+import pandas as pd
+import random
 
-# Download latest version
-path = kagglehub.dataset_download("masoudnickparvar/brain-tumor-mri-dataset")
+kaggle.api.authenticate()
 
-print("Path to dataset files:", path)
+kaggle.api.dataset_download_files("masoudnickparvar/brain-tumor-mri-dataset",path='.',unzip=True)
+
+kaggle.api.dataset_metadata("masoudnickparvar/brain-tumor-mri-dataset",path='.')
+
+print(kaggle.api.dataset_list_files("masoudnickparvar/brain-tumor-mri-dataset").files)
